8e20967e8dca542b28aa8eee21ee5de2ae8e9c0d
DPLB <diego.leal94@outlook.com>
2023-04-10 18:15:35 -0400

    Fix

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -12,15 +12,20 @@
             <canvas id="myChart"></canvas>
             <?php if (!empty($fecha)): ?>
             <div style="margin-left: 5px;">
-                <input type='date' onchange='startDateFilter(this)' value='{{ $fecha[0] }}' min='{{ $fecha[0] }}' max='{{ $fecha[count($fecha)-1] }}'>
-                <input type='date' onchange='endDateFilter(this)' value='{{ $fecha[count($fecha)-1] }}' min='{{ $fecha[0] }}' max='{{ $fecha[count($fecha)-1] }}'>
+                <input type='date' onchange='startDateFilter(this)' value='{{ $fecha[0] }}' min='{{ $fecha[0] }}'
+                    max='{{ $fecha[count($fecha) - 1] }}'>
+                <input type='date' onchange='endDateFilter(this)' value='{{ $fecha[count($fecha) - 1] }}'
+                    min='{{ $fecha[0] }}' max='{{ $fecha[count($fecha) - 1] }}'>
             </div>
+            <?php else: ?>
+            <p>No se encontraron datos para formar el gráfico</p>
             <?php endif; ?>
             <br>
         </div>
     </div>
 @endsection
 
+<?php if (!empty($fecha)): ?>
 @section('scripts')
     @parent
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js/dist/chart.umd.min.js"></script>
@@ -28,12 +33,11 @@
     </script>
 
     <script>
-
-    // Se definen las variables "valores" y "fechas" con los valores de la BD
+        // Se definen las variables "valores" y "fechas" con los valores de la BD
         var valores = <?= json_encode($valor) ?>;
         var fechas = <?= json_encode($fecha) ?>;
 
-    // Se define el objeto "data" que especifica los datos del gráfico de línea
+        // Se define el objeto "data" que especifica los datos del gráfico de línea
         const data = {
             labels: fechas,
             datasets: [{
@@ -47,7 +51,7 @@
             }]
         };
 
-    // Se define el objeto "config" que especifica la configuración del gráfico de línea
+        // Se define el objeto "config" que especifica la configuración del gráfico de línea
         const config = {
             type: 'line',
             data,
@@ -55,7 +59,7 @@
                 scales: {
                     x: {
                         min: '{{ $fecha[0] }}',
-                        max: '{{ $fecha[count($fecha)-1] }}',
+                        max: '{{ $fecha[count($fecha) - 1] }}',
                         type: 'time',
                     }
                 },
@@ -63,24 +67,25 @@
             }
         };
 
-    // Se crea el objeto Chart y se muestra en el elemento con ID "myChart"
+        // Se crea el objeto Chart y se muestra en el elemento con ID "myChart"
         const myChart = new Chart(
             document.getElementById('myChart'),
             config
         );
 
-    // Función que se llama cuando se cambia la fecha de inicio del input
+        // Función que se llama cuando se cambia la fecha de inicio del input
         function startDateFilter(date) {
             const startDate = new Date(date.value);
             myChart.config.options.scales.x.min = startDate;
             myChart.update();
         }
 
-    // Función que se llama cuando se cambia la fecha de fin del input
+        // Función que se llama cuando se cambia la fecha de fin del input
         function endDateFilter(date) {
             const endDate = new Date(date.value);
             myChart.config.options.scales.x.max = endDate;
             myChart.update();
         }
     </script>
+    <?php endif; ?>
 @stop
